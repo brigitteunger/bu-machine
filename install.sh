b9a17f2a06720387afd06278479f88ae8ca6e533
brigi <58770308+brigitteunger@users.noreply.github.com>
2020-11-29 18:40:48 +0100

    Add more libs

--- install.sh
+++ install.sh
@@ -10,14 +10,19 @@ apt-get -y install nautilus-dropbox
 apt-get -y install keepassxc
 apt-get -y install python3.8
 apt-get -y install python3-pip
+apt-get -y install python3-tk
 pip3 install virtualenv
+apt-get -y install python3-venv
 apt-get -y install vlc
 apt-get -y install ffmpeg
 apt-get -y install jabref
+apt-get -y install libx11-dev libatlas-base-dev
+apt-get -y install libgtk-3-dev libboost-python-dev
 
 snap install code --classic
 snap install cmake --classic
 snap install slack --classic
+
 
 wget https://teams.microsoft.com/downloads/desktopurl?env=production&plat=linux&arch=x64&download=true&linuxArchiveType=deb
 
@@ -30,5 +35,4 @@ EOF
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
 sudo apt-get update
 sudo apt-get install pritunl-client-electron
-
 
